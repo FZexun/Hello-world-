8fb16eb1871a447034ebefd712ef001b92f85f81
FZexun <34880041+FZexun@users.noreply.github.com>
2018-01-14 23:01:02 +0800

    Add files via upload

---
+++ Day 2-2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#_*_ coding: utf-8_*_
+
+counter = 100 # 赋值整形变量
+miles = 1000.0 # 浮点型
+name = 'FZexun' # 字符型
+
+print(counter)
+print (miles)
+print(name)
+
+x=1
+y=2
+z=x + y
+print(x)
+print(y)
+print(z)
+print(x,y,z)
+
+str = 'Hello World!'
+print (str)            #输出完整字符串
+print (str[0])         #输出字符串中的第一个字符
+print (str[2:5])       #输出字符串中第三个至第五个之间的字符串
+print (str[2:])        #输出从第三个字符开始的字符串
+print (str * 2)        #输出字符串两次
+print (str + 'Test')   #输出连接的字符串
+
