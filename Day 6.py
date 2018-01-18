3b50fda0e0a616b1cb62f09f93c4de4acba03201
FZexun <34880041+FZexun@users.noreply.github.com>
2018-01-18 18:46:09 +0800

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# _ * _ coding: utf-8 _ * _
+
+# 例1：if基本用法
+
+flag = False
+name = 'luren'
+if name == 'python' :           #判断变量否为'python'
+    flag = True                 #条件成立时设置标志为真
+    print('weicome boss!')      #并输出欢迎信息
+
+else:
+    print(name)                 #条件不成立时输出变量名称
