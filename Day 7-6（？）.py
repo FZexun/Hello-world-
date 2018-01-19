4339c84b178e1453731ee62ae457c7c23e545ec7
FZexun <34880041+FZexun@users.noreply.github.com>
2018-01-19 17:26:21 +0800

    Add files via upload

---
+++ Day 7-6（？）.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# _ * _ coding: utf-8 _ *＿
+
+#十进制转二进制
+
+denum = input('输入十进制数：')
+
+print (denum, '(10)',)
+binnum = []
+#二进制
+while denum() > 0:
+    binnum.append(str(denum % 2))  #栈压入
+    denum //= 2
+print('= ',)
+while len(binnum)>0:
+    import sys
+    sys.stdout.write(binnum.pop()) #无空格输出print '(2)'
+
