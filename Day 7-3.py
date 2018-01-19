1a9cfea24f2b682c71e129284e1fc6f7c388e1b7
FZexun <34880041+FZexun@users.noreply.github.com>
2018-01-19 17:24:58 +0800

    Add files via upload

---
+++ Day 7-3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# _ * _ coding: utf-8 _ * _
+
+count = 0
+while count < 5:
+    print(count,'is less than 5')
+    count = count + 1
+else:
+    print(count,'is not less than 5')
+    
+print('--------------------------------')
+flag = 1
+while(flag):print('given flag is really true!')
+print ('goodbye')
